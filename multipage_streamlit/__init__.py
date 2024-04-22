1af439177122a019b2be6ca6f4f979e481b63b67
Ankesh Kumar <43654500+ankesh86@users.noreply.github.com>
2024-04-22 18:29:19 -0400

    __init__.py

--- multipage_streamlit/__init__.py
+++ multipage_streamlit/__init__.py
@@ -1,6 +1,2 @@
-# read version from installed package
-from importlib.metadata import version
-__version__ = version("multipage_streamlit")
-
 from .multipage import MultiPage
 from .state import State
