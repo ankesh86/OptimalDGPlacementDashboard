32801097d061b49ab9a0598d5f4f61bf1cc07a2c
Ankesh Kumar <ak.email86@gmail.com>
2024-04-22 18:34:42 -0400

    deleted:    PowerDash.py deleted:    PowerDashPages/DGplacement.py deleted:    PowerDashPages/Data/output16bus.csv deleted:    PowerDashPages/Data/output16busBusdata.csv deleted:    PowerDashPages/Data/output16busLinedata.csv deleted:    PowerDashPages/Data/output33bus.csv deleted:    PowerDashPages/Data/output33busBusdata.csv deleted:    PowerDashPages/Data/output33busLinedata.csv deleted:    PowerDashPages/Data/output69bus.csv deleted:    PowerDashPages/Data/output69busBusdata.csv deleted:    PowerDashPages/Data/output69busLinedata.csv deleted:    PowerDashPages/Data2/output16bus_PLoss.csv deleted:    PowerDashPages/Data2/output16bus_QLoss.csv deleted:    PowerDashPages/Data2/output16bus_SLoss.csv deleted:    PowerDashPages/Data2/output16bus_VSI.csv deleted:    PowerDashPages/Data2/output16bus_VtgDev.csv deleted:    PowerDashPages/Data2/output33bus_PLoss.csv deleted:    PowerDashPages/Data2/output33bus_QLoss.csv deleted:    PowerDashPages/Data2/output33bus_SLoss.csv deleted:    PowerDashPages/Data2/output33bus_VSI.csv deleted:    PowerDashPages/Data2/output33bus_VtgDev.csv deleted:    PowerDashPages/Data2/output69bus_PLoss.csv deleted:    PowerDashPages/Data2/output69bus_QLoss.csv deleted:    PowerDashPages/Data2/output69bus_SLoss.csv deleted:    PowerDashPages/Data2/output69bus_VSI.csv deleted:    PowerDashPages/Data2/output69bus_VtgDev.csv deleted:    PowerDashPages/Data3/output16bus_PLoss.csv deleted:    PowerDashPages/Data3/output33bus_PLoss.csv deleted:    PowerDashPages/Data3/output69bus_PLoss.csv deleted:    PowerDashPages/__pycache__/DGplacement.cpython-310.pyc deleted:    PowerDashPages/__pycache__/networkdetails.cpython-310.pyc deleted:    PowerDashPages/__pycache__/optimalloc.cpython-310.pyc deleted:    PowerDashPages/networkdetails.py deleted:    PowerDashPages/optimalloc.py deleted:    README.md deleted:    __init__.py deleted:    multipage.py deleted:    multipage_streamlit/__init__.py deleted:    multipage_streamlit/__pycache__/__init__.cpython-310.pyc deleted:    multipage_streamlit/__pycache__/multipage.cpython-310.pyc deleted:    multipage_streamlit/__pycache__/state.cpython-310.pyc deleted:    multipage_streamlit/multipage.py deleted:    multipage_streamlit/state.py deleted:    src/multipage_streamlit/__init__.py deleted:    src/multipage_streamlit/multipage.py deleted:    src/multipage_streamlit/state.py deleted:    state.py

--- src/multipage_streamlit/__init__.py
+++
@@ -1,6 +0,0 @@
-# read version from installed package
-from importlib.metadata import version
-__version__ = version("multipage_streamlit")
-
-from .multipage import MultiPage
-from .state import State
